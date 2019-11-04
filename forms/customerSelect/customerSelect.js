a7bc3cb2914751900143759b2dd6160b4a6b4b62
Priyanka Gupta <pgu04554@creighton.edu>
2019-11-03 21:06:27 -0600

    completed

---
+++ forms/customerSelect/customerSelect.js
@@ -0,0 +1,42 @@
+customerSelect.onshow=function(){
+   let query = "SELECT state FROM customer"
+    req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=pgu04554&pass=Priyanka98*&database=pgu04554&query=" + query)
+  if (req1.status == 200) { 
+
+        results = JSON.parse(req1.responseText)
+  if (results.length == 0)
+        NSB.MsgBox("There are no states of that type.") 
+    else {        
+        var message = ""
+        for (i = 0; i <= results.length - 1; i++)
+            message = message + results[i][0] + "\n"
+        txtState.value = message
+      } 
+
+  } else
+
+        NSB.MsgBox("Error code: " + req1.status)
+}
+
+btnSubmit.onclick=function(){
+  let state = inptState.value
+  let query = "SELECT name FROM customer WHERE state = " + '"' + state + '"'
+
+
+    req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=pgu04554&pass=Priyanka98*&database=pgu04554&query=" + query)
+  if (req1.status == 200) { 
+
+        results = JSON.parse(req1.responseText)
+  if (results.length == 0)
+        NSB.MsgBox("There are no states of that type.") 
+    else {        
+        var message = ""
+        for (i = 0; i <= results.length - 1; i++)
+            message = message + results[i][0] + "\n"
+        txtResults.value = message
+      } 
+
+  } else
+
+        NSB.MsgBox("Error code: " + req1.status)
+}
---
+++ forms/customerUpdate/customerUpdate.js
@@ -0,0 +1,43 @@
+
+customerUpdate.onshow=function(){
+  let query = "SELECT name FROM customer"
+    req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=pgu04554&pass=Priyanka98*&database=pgu04554&query=" + query)
+  if (req1.status == 200) { 
+
+        results = JSON.parse(req1.responseText)
+  if (results.length == 0)
+        NSB.MsgBox("There are no names of that type.") 
+    else {        
+        var message = ""
+        for (i = 0; i <= results.length - 1; i++)
+            message = message + results[i][0] + "\n"
+        txtNames.value = message
+      } 
+
+  } else
+
+        NSB.MsgBox("Error code: " + req1.status)
+}
+
+
+inptSubmit11.onclick=function(){
+  var newName = inptNew.value
+  var oldName = inptOld.value
+  var query = "UPDATE customer SET name =" + '"' + newName + '"' + " WHERE name = " + '"' + oldName + '"'
+
+      req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=pgu04554&pass=Priyanka98*&database=pgu04554&query=" + queryUpdate)
+
+    if (req1.status == 200) { 
+    if (req1.responseText == 500) 
+            NSB.MsgBox("You have successfully updated the name: " + newName)
+            var message = ""
+          for (i = 0; i <= results.length - 1; i++)
+            message = message + results[i][0] + "\n"
+        txtResults11.value = message
+        else
+            NSB.MsgBox("There was a problem updating " + oldName + " from the database.")
+      } else {
+
+        NSB.MsgBox("Error: " + req1.status);
+      }  
+}
